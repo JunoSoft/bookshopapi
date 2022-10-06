b336b9fe864aca443f740ced7d1e087200c1922f
Junosoft <junosoftltd@gmail.com>
2022-10-06 21:51:55 +0200

    first commit

--- routes/API.js
+++ routes/API.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router  = express.Router();
 const book = require("../controllers/book");
 
-router.post("/addbook",book.addBook);
+router.post("/addbook",);
 
 
 module.exports = router
